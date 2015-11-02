cad6302fcc4c426395722c3e15705d53e77bcc06
twosheep0603 <mike830603@gmail.com>
2015-11-03 02:09:34 +0800

    White Back Update

--- javascripts/main.js
+++ javascripts/main.js
@@ -20,15 +20,19 @@ $(function(){
     var subtitle = $('#subtitle').val();
     var big = $('#big').val();
     var word = $('#word').val();
-    ctx.clearRect(0,0,800,500);
+    ctx.clearRect(0,0,800,380);
+    ctx.globalCompositeOperation = 'source-over';
+    
     var size1 = title.length<3?168:Math.floor(500/title.length)
     var size2 = subtitle.length<6?50:Math.floor(300/subtitle.length)
     var size3 = Math.floor(48+48/big.length)
     var size4 = word.length<5?52:Math.floor(32+100/word.length)
+    
+    ctx.fillStyle="#000";
     ctx.font = "900 "+size1+"px 'Open Sans'";
     ctx.textAlign='center';
     ctx.fillText(title,220,150+size1/2);
-    ctx.globalCompositeOperation = 'source-in';
+    ctx.globalCompositeOperation = 'source-atop';
     ctx.drawImage(img,-10,20,520,360);
     ctx.globalCompositeOperation = 'source-over';
     ctx.font = "400 "+size2+"px 'Open Sans'";
@@ -41,6 +45,9 @@ $(function(){
     ctx.fillText(big,450,250-size1/2);
     ctx.font = "400 "+size4+"px 'Open Sans'";
     ctx.fillText(word,450,310-size1/2);
+    ctx.globalCompositeOperation='destination-atop';
+    ctx.fillStyle="#FFF";
+    ctx.fillRect(0,0,800,380);
   }
   function downloadCanvas(link, canvasID, filename) {
     link.href = document.getElementById(canvasID).toDataURL('image/png');
